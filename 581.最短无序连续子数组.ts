2ad8c25013c99c964ce9675ad730e2c60c51d2e3
Black <827187153@qq.com>
2022-08-18 08:28:27 +0800

    init

---
+++ 581.最短无序连续子数组.ts
@@ -0,0 +1,13 @@
+/*
+ * @lc app=leetcode.cn id=581 lang=typescript
+ *
+ * [581] 最短无序连续子数组
+ */
+
+// @lc code=start
+function findUnsortedSubarray(nums: number[]): number {
+    const a:number = 0;
+    return a;
+};
+// @lc code=end
+
